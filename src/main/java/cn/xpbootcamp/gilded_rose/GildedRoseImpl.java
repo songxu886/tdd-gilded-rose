6b9cacc72857e982b1da552db96b89c85422b5ce
Song Xu <song.xu@thoughtworks.com>
2020-03-23 21:42:47 +0800

    add Backstage pass test and implement

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRoseImpl.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRoseImpl.java
@@ -14,12 +14,28 @@ public class GildedRoseImpl implements GildedRose {
                     increaseQuality(product);
                 case "Sulfuras":
                     break;
+                case "Backstage pass":
+                    increaseBackstagePassQuality(product);
             }
 
             if (!product.getName().equals("Sulfuras")) {
                 product.setSellIn(product.getSellIn() - 1);
             }
         });
+    }
+
+    private void increaseBackstagePassQuality(Product product) {
+        int sellIn = product.getSellIn();
+        int quality = product.getQuality();
+        if (sellIn <= 10 && sellIn > 5) {
+            product.setQuality(quality + 2);
+        } else if (sellIn > 10) {
+            product.setQuality(quality + 1);
+        } else if (sellIn > 1 && sellIn <= 5) {
+            product.setQuality(quality + 3);
+        } else if (sellIn == 1) {
+            product.setQuality(0);
+        }
     }
 
     private void increaseQuality(Product product) {
--- src/test/java/cn/xpbootcamp/gilded_rose/GildedRoseTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/GildedRoseTest.java
@@ -61,4 +61,39 @@ public class GildedRoseTest {
         assertEquals(51, items.get(2).getQuality());
         assertEquals(2, items.get(2).getSellIn());
     }
+
+    @Test
+    public void testUpdateQualityOfBackstagePass() {
+        // Given
+        List<Product> items = Stream
+                .of(new Product("Backstage pass", 10, 5),
+                        new Product("Backstage pass", 5, 5),
+                        new Product("Backstage pass", 11, -1),
+                        new Product("Backstage pass", 2, 51),
+                        new Product("Backstage pass", 11, 3),
+                        new Product("Backstage pass", 1, 35))
+                .collect(Collectors.toList());
+
+        // When
+        gildedRose.updateQuality(items);
+
+        // Then
+        assertEquals(7, items.get(0).getQuality());
+        assertEquals(9, items.get(0).getSellIn());
+
+        assertEquals(8, items.get(1).getQuality());
+        assertEquals(4, items.get(1).getSellIn());
+
+        assertEquals(-1, items.get(2).getQuality());
+        assertEquals(11, items.get(2).getSellIn());
+
+        assertEquals(51, items.get(3).getQuality());
+        assertEquals(2, items.get(3).getSellIn());
+
+        assertEquals(4, items.get(4).getQuality());
+        assertEquals(10, items.get(4).getSellIn());
+
+        assertEquals(0, items.get(5).getQuality());
+        assertEquals(0, items.get(5).getSellIn());
+    }
 }
